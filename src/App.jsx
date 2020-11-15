7148d428514adc45e4dcef5f273f7c30fc95ca9c
Sophie Roe <sophie.roe@Nology.local>
2020-11-15 18:55:49 +0000

    added project

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,21 @@ import SubmitForm from './components/SubmitForm';
 
 class App extends Component {
   state = {
-    tasks: ['wash dishes', 'wash car', 'food shopping']
+    tasks: ['wash dishes', 'wash car', 'food shopping'],
+    name: "",
+    picture: "",
+    city: ""
     }
 
+    async componentDidMount() {
+      const url ="https://randomuser.me/api/ "
+      const response = await fetch(url);
+      const data = await response.json();
+
+      this.setState({name:data.results[0].name.first})
+      this.setState({picture:data.results[0].picture.large})
+      this.setState({city:data.results[0].location.city})
+    }
     handleDelete = (index) => {
       const newArray = [...this.state.tasks];
       newArray.splice(index,1);
@@ -23,7 +35,7 @@ class App extends Component {
       <div className={styles.page}>
     <div className={styles.container}>
       <h1>To Do List</h1>
-      <Header tasks={this.state.tasks.length} />
+      <Header tasks={this.state.tasks.length} name={this.state.name} picture={this.state.picture} city={this.state.city} />
       <TodoList 
       tasks={this.state.tasks}
       onDelete={this.handleDelete}/>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,34 @@ import React, { Component } from "react";
 import styles from "./Header.module.scss";
 
 class Header extends Component {
-  render() {
+  // state = {
+    
+  // };
+
+  // componentDidMount() {
+  //   this.setRandomUser();
+  // }
+
+  // setRandomUser = () => {
+  //   // 4. Do a fetch
+  //   fetch('https://randomuser.me/api/')
+  //       .then(response => response.json())
+  //       .then(response => {
+  //       ;
+  //         this.setState({ randomUser: response[0].name.first});
+  //         console.log(response)
+  //       });}
+
+     
+
+        render() {
+          // const {randomUser} = this.state
     return (
+      
       <>
-        <p>You have <span className={styles.number}>{this.props.tasks}</span> to dos left to do </p>
+      <img src={this.props.picture} alt=""/>
+    
+        <p><span>{this.props.name}</span> you have <span className={styles.number}>{this.props.tasks}</span> to dos left to do in {this.props.city} </p>
       </>
     );
   }
