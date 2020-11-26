706b1882fa42e33316379c8a30ba654c445a4546
Sophie Roe <sophie.roe@Nology.local>
2020-11-26 14:53:16 +0000

    responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {Component } from 'react';
+import React, { Component } from 'react';
 import Header from './components/Header';
 import styles from "./App.module.scss";
 import TodoList from './components/TodoList';
@@ -10,40 +10,40 @@ class App extends Component {
     name: "",
     picture: "",
     city: ""
-    }
+  }
 
-    async componentDidMount() {
-      const url ="https://randomuser.me/api/ "
-      const response = await fetch(url);
-      const data = await response.json();
+  async componentDidMount() {
+    const url = "https://randomuser.me/api/ "
+    const response = await fetch(url);
+    const data = await response.json();
 
-      this.setState({name:data.results[0].name.first})
-      this.setState({picture:data.results[0].picture.large})
-      this.setState({city:data.results[0].location.city})
-    }
-    handleDelete = (index) => {
-      const newArray = [...this.state.tasks];
-      newArray.splice(index,1);
-      this.setState({tasks:newArray});
-    }
+    this.setState({ name: data.results[0].name.first })
+    this.setState({ picture: data.results[0].picture.large })
+    this.setState({ city: data.results[0].location.city })
+  }
+  handleDelete = (index) => {
+    const newArray = [...this.state.tasks];
+    newArray.splice(index, 1);
+    this.setState({ tasks: newArray });
+  }
 
-    handleSubmit = (task) => {
-      this.setState({tasks:[...this.state.tasks, task]})
-    }
-  render() { 
+  handleSubmit = (task) => {
+    this.setState({ tasks: [...this.state.tasks, task] })
+  }
+  render() {
     return (
       <div className={styles.page}>
-    <div className={styles.container}>
-      <h1>To Do List</h1>
-      <Header tasks={this.state.tasks.length} name={this.state.name} picture={this.state.picture} city={this.state.city} />
-      <TodoList 
-      tasks={this.state.tasks}
-      onDelete={this.handleDelete}/>
-      <SubmitForm onFormSubmit={this.handleSubmit}/>
-    </div>
-    </div>
+        <div className={styles.container}>
+          <h1>To Do List</h1>
+          <Header tasks={this.state.tasks.length} name={this.state.name} picture={this.state.picture} city={this.state.city} />
+          <TodoList
+            tasks={this.state.tasks}
+            onDelete={this.handleDelete} />
+          <SubmitForm onFormSubmit={this.handleSubmit} />
+        </div>
+      </div>
     );
   }
 }
- 
+
 export default App;
